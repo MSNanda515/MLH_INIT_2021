06cdee4eb18f150cbd488223064e1bd8bc17d38d
Meharpreet Singh Nanda <msnanda515@gmail.com>
2021-07-10 14:36:58 -0600

    Added Basic Logic to the Ui

--- InitCalculator/MainPage.xaml.cs
+++ InitCalculator/MainPage.xaml.cs
@@ -10,9 +10,29 @@ namespace InitCalculator
 {
     public partial class MainPage : ContentPage
     {
+        private double firstNo, secondNo;
+        int state;
         public MainPage()
         {
             InitializeComponent();
+            OnClear(this, null);
         }
+
+        private void OnClear(object sender, EventArgs e)
+        {
+            firstNo = 0;
+            secondNo = 0;
+            state = 1;
+            this.Result.Text = "0";
+        }
+
+        private void NumberClicked(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            int oldNo = Int32.Parse(this.Result.Text);
+            int newNo = Int32.Parse(button.Text);
+            this.Result.Text = (oldNo + newNo).ToString();
+        }
+        
     }
 }

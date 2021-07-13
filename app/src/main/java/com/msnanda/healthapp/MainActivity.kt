50d8b48ee1dee2d77701f5509e4ee1d0a6a4ed9d
Meharpreet Singh Nanda <msnanda515@gmail.com>
2021-07-13 16:51:34 -0600

    Basic UI in place

--- app/src/main/java/com/msnanda/healthapp/MainActivity.kt
+++ app/src/main/java/com/msnanda/healthapp/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.msnanda.healthapp
 
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener {
+
+    var running = false;
+    var sensorManager: SensorManager? = null;
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        sensorManager = getSystemService(Context.Sens)
     }
+
+    override fun onResume() {
+        super.onResume();
+    }
+
+    override fun onPause() {
+        super.onPause();
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+    }
+
+
 }
